c6e9e429faccab89c681e110388ebfce2cde4516
drithh <adrielalfeus@gmail.com>
2024-08-30 12:46:30 +0700

    add cloudflare pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'site-assets.plasmic.app',
+      },
+    ],
+  },
+  trailingSlash: true,
 };
-
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 export default nextConfig;
