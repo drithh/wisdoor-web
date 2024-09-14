a036c5ef39e950ea181a055ea3c30bc4e6930950
drithh <adrielalfeus@gmail.com>
2024-09-14 14:23:02 +0700

    remote opts

---
+++ remoteOptimizedImages.js
@@ -0,0 +1,6 @@
+module.exports = [
+  'https://site-assets.plasmic.app/ac8b44f7fbf5a81c839a180920ea249c.png',
+  'https://site-assets.plasmic.app/1818fb887829971355e6af8eee65fc82.png',
+  'https://site-assets.plasmic.app/ec7615efd9eec59506460977dde021a8.jpg',
+  'https://site-assets.plasmic.app/843daac37ed39e5c0fd4392b69506628.jpg',
+];
