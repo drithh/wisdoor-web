6e52f594e02f934e76ece263ca719e2ce52a568f
drithh <adrielalfeus@gmail.com>
2024-10-28 20:08:12 +0700

    remove div wrapper

--- components/door/components/configurator-wrapper.tsx
+++ components/door/components/configurator-wrapper.tsx
@@ -13,13 +13,12 @@ export function ConfiguratorWrapper({ className, children }: WrapperProps) {
       id="configurator-wrapper"
       className={cn(
         `fixed sm:static overflow-auto bottom-0 sm:h-full h-[calc(100dvh-16rem)] w-dvw sm:w-[467px] sm:min-w-[467px]`,
+        `sm:mt-[4rem] sm:ml-[0.5rem] sm:mr-[4.5rem]`,
         className
       )}
     >
-      <div className="sm:mt-[4rem] sm:ml-[0.5rem] sm:mr-[4.5rem]">
-        <div className="sm:hidden fixed top-[16rem] left-0 m-0 z-[1000] inset-0 w-full h-20 bg-gradient-to-t from-transparent to-white pointer-events-none" />
-        <div className="flex gap-10 flex-col sm:p-0 p-4 z-0">{children}</div>
-      </div>
+      <div className="sm:hidden fixed top-[16rem] left-0 m-0 z-[1000] inset-0 w-full h-20 bg-gradient-to-t from-transparent to-white pointer-events-none" />
+      <div className="flex gap-10 flex-col sm:p-0 p-4 z-0">{children}</div>
     </div>
   );
 }
