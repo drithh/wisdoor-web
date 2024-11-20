960fcbb9e92ed070b76840b5033a97a71ccbd42c
Adriel Alfeus <adrielalfeus@gmail.com>
2024-11-20 08:54:35 +0700

    fix image opt

--- next.config.mjs
+++ next.config.mjs
@@ -8,8 +8,8 @@ const nextConfig = {
   },
   images: {
     loader: 'custom',
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [32, 96, 128, 256],
+    deviceSizes: [320, 640, 1080, 1920],
     remotePatterns: [
       {
         protocol: 'https',
